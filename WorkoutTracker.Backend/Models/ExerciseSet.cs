094674a860a0142b0cc8b1a23c1c489d71d667ce
Rizqial5 <bagusriski1@gmail.com>
2024-12-13 11:49:28 +0700

    Revise Database Schema

---
+++ WorkoutTracker.Backend/Models/ExerciseSet.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutTracker.Backend.Models
+{
+    public class ExerciseSet
+    {
+        [Key]
+        public int ExerciseSetId { get; set; }
+
+        [Required]
+        public int Set { get; set; }
+
+        [Required]
+        public int Repetitions { get; set; }
+
+        [Required]
+        public string Weight { get; set; }
+
+        [ForeignKey("ExerciseId")]
+        public int ExerciseId { get; set; }
+
+        public ExerciseData Exercise { get; set; }
+
+    }
+}
