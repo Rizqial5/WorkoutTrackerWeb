6de6a1abdb03e6bb65f13a456ca3fb3ab3e4f989
Rizqial5 <bagusriski1@gmail.com>
2024-12-20 18:23:31 +0700

    Refactoring GetWorkoutPlans Unit Testing

--- WorkoutTracker.Tests/WorkoutPlanControllerTest.cs
+++ WorkoutTracker.Tests/WorkoutPlanControllerTest.cs
@@ -24,10 +24,17 @@ public class WorkoutPlanControllerTest
     private readonly UserManager<IdentityUser> _userManager;
     private readonly IRedisCacheService _redisCacheService;
     private readonly ILogger<WorkoutPlansController> _logger;
+    private readonly ApplicationDbContext fakeDbContext;
 
     public WorkoutPlanControllerTest(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase").Options;
+
+        fakeDbContext = A.Fake<ApplicationDbContext>(x =>
+            x.WithArgumentsForConstructor(() => new ApplicationDbContext(options)));
+
         _userManager = A.Fake<UserManager<IdentityUser>>();
         _redisCacheService = A.Fake<IRedisCacheService>();
         _logger = A.Fake<ILogger<WorkoutPlansController>>();
@@ -36,15 +43,45 @@ public class WorkoutPlanControllerTest
     [Fact]
     public async Task GetWorkoutPlans_ReturnsDataWithUserLogin()
     {
-        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase").Options;
-
-        //var _context = new ApplicationDbContext(options);
 
         // Arrange
 
-
         var identityUser = new IdentityUser { Id = "User1", UserName = "TestUser" };
+        var workoutPlans = GenerateWorkoutPlansList(identityUser);
+
+        A.CallTo(() => fakeDbContext.WorkoutPlans).Returns(workoutPlans.AsQueryable().BuildMockDbSet());
+
+        // Fake UserManager
+        var fakeUser = identityUser;
+        A.CallTo(() => _userManager.GetUserAsync(A<ClaimsPrincipal>.Ignored))
+            .Returns(Task.FromResult(fakeUser));
+
+        // Fake CacheService
+        A.CallTo(() => _redisCacheService.GetCacheAsync<List<WorkoutPlanResponse>>(A<string>.Ignored))
+            .Returns(Task.FromResult<List<WorkoutPlanResponse>>(null)); // Simulating no cache hit
+
+        
+
+        var controller = new WorkoutPlansController(fakeDbContext, _userManager, _redisCacheService, _logger);
+
+        // Act
+        var result = await controller.GetWorkoutPlans();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var response = Assert.IsAssignableFrom<IEnumerable<WorkoutPlanResponse>>(okResult.Value);
+
+        
+        // Verifying the response
+          // We expect one response due to the single fake WorkoutPlan
+        var planResponse = response.First();
+
+        Assert.Equal("Plan A", planResponse.PlanName);
+        Assert.Equal("TestUser", planResponse.User); // Check fake user login
+    }
+
+    private static List<WorkoutPlans> GenerateWorkoutPlansList(IdentityUser identityUser)
+    {
         var workoutPlans = new List<WorkoutPlans>
         {
             new WorkoutPlans
@@ -66,53 +103,6 @@ public class WorkoutPlanControllerTest
                 ExerciseSets = new List<ExerciseSet>()
             }
         };
-
-
-        
-        var fakeDbContext = A.Fake<ApplicationDbContext>(x =>
-            x.WithArgumentsForConstructor(()=> new ApplicationDbContext(options)));
-
-        
-        A.CallTo(() => fakeDbContext.WorkoutPlans).Returns(workoutPlans.AsQueryable().BuildMockDbSet());
-
-        // Fake UserManager
-        var fakeUser = identityUser;
-        A.CallTo(() => _userManager.GetUserAsync(A<ClaimsPrincipal>.Ignored))
-            .Returns(Task.FromResult(fakeUser));
-
-
-
-        // Fake CacheService
-        A.CallTo(() => _redisCacheService.GetCacheAsync<List<WorkoutPlanResponse>>(A<string>.Ignored))
-            .Returns(Task.FromResult<List<WorkoutPlanResponse>>(null)); // Simulating no cache hit
-
-        // Fake query Calling
-
-        var controller = new WorkoutPlansController(fakeDbContext, _userManager, _redisCacheService, _logger);
-
-        // Act
-        var result = await controller.GetWorkoutPlans();
-
-        // Assert
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var response = Assert.IsAssignableFrom<IEnumerable<WorkoutPlanResponse>>(okResult.Value);
-
-        
-        // Verifying the response
-          // We expect one response due to the single fake WorkoutPlan
-        var planResponse = response.First();
-        var planResponseList = response.ToList();
-
-        Assert.Equal("Plan A", planResponse.PlanName);
-        Assert.Equal("TestUser", planResponse.User);
-
-        foreach (var item in planResponseList)
-        {
-            _testOutputHelper.WriteLine($"Plan Name: {item.PlanName}, User: {item.User}");
-        }
-
-        // Output to check what's returned
-        _testOutputHelper.WriteLine($"Plan Name: {planResponse.PlanName}, User: {planResponse.User}");
+        return workoutPlans;
     }
-
 }
