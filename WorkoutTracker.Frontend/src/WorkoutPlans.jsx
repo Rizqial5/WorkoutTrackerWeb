311b67ec3f3c5ac26715fdc02fbb7458489bb108
Rizqial5 <bagusriski1@gmail.com>
2024-12-28 21:36:18 +0700

    Add WorkoutPlans.jsx

---
+++ WorkoutTracker.Frontend/src/WorkoutPlans.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import axios from './backend/axios';
+
+
+function WorkoutPlans() {
+  return (
+    <p>Hello world!</p>
+  );
+}
+
+export default WorkoutPlans;
--- WorkoutTracker.Frontend/src/main.jsx
+++ WorkoutTracker.Frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import WorkoutPlans from './WorkoutPlans.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    < WorkoutPlans/>
   </StrictMode>,
 )
