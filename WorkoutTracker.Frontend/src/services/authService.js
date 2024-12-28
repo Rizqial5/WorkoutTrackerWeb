fd70b3232a4aeba0ba5a3b1aa485ac6f6817456d
Rizqial5 <bagusriski1@gmail.com>
2024-12-28 21:50:13 +0700

    Implement authService in WorkoutTracker.Frontend

--- WorkoutTracker.Frontend/src/services/authService.js
+++ WorkoutTracker.Frontend/src/services/authService.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import axios from 'axios'
+
+const API_URL = '/api/auth';
+
+export const login = async (email, password) => {
+    try {
+        const response = await axios.post(`${API_URL}/login`, { email, password });
+        const { token } = response.data;
+        localStorage.setItem('token', token);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || 'Login failed';
+    }
+};
+
+export const logout = () => {
+    localStorage.removeItem('token');
+}
+
+export const isAuthenticated= () => {
+    return !!localStorage.getItem('token');
+};
