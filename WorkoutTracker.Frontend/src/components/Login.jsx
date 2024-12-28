a240d93764927d7e4cf53dc442fcf5ba2b9b72a3
Rizqial5 <bagusriski1@gmail.com>
2024-12-28 22:04:11 +0700

    Implement Login.jsx in comonent WorkoutTracker.Frontend

---
+++ WorkoutTracker.Frontend/src/components/Login.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { login } from './services/authService'
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault;
+        setError('');
+        try {
+            await login(email, password);
+            navigate('/main');
+        } catch (err) {
+            setError(err || 'Failed to Login');
+        }
+    };
+
+    return (
+        <div style={{ maxWidth: '400px', margin: '50px auto' }}>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin()}>
+                <input
+                    type="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required/>
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required/>
+                <button type="submit">Login</button>
+            </form>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+        </div>
+    );
+};
+
+export default Login;
