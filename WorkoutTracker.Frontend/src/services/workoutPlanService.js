e9421223c7f8c465e6aaa5ce88c3313aa192f214
Rizqial5 <bagusriski1@gmail.com>
2024-12-28 23:43:39 +0700

    Add workoutPlanService in services WorkoutTracker.Frontend

--- WorkoutTracker.Frontend/src/services/workoutPlanService.js
+++ WorkoutTracker.Frontend/src/services/workoutPlanService.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import axios from 'axios'
+
+const API_URL = '/api/workoutplans';
+
+const token = localStorage.getItem('token');
+
+export const getWorkoutPlans = async () => {
+
+    
+
+    try {
+        const response = await axios.get(`${API_URL}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || 'Get Plans Failed';
+    }
+}
